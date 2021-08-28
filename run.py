f60df92a27053db8901eb09eac87bb9907fef93c
Ignacio Vizzo <ignaciovizzo@gmail.com>
2021-08-28 20:14:01 +0200

    Migrate folder names

--- run.py
+++ run.py
@@ -2,13 +2,12 @@
 import hydra
 from omegaconf import DictConfig
 
-# Project Imports
 from slam.odometry.odometry_runner import SLAMRunner, SLAMRunnerConfig
 
 
 @hydra.main(config_path="config", config_name="slam")
 def run_slam(cfg: DictConfig) -> None:
-    """The main entry point to the script running the SLAM"""
+    """The main entry point to the script running the SLAM."""
     _odometry_runner = SLAMRunner(SLAMRunnerConfig(**cfg))
     _odometry_runner.run_odometry()
 
